cd42bb13ed7896b14b6acad4d5e5dca2d903d645
sofiyareverse <sofiya.yurova@gmail.com>
2018-02-06 22:46:26 +0200

    pifagor ebanii

--- task.rb
+++ task.rb
@@ -1,53 +1,57 @@
 # Идеальный вес
 puts 'Ваше имя?'
-username = gets.chomp
+username = gets
 puts 'Ваш вес?'
-weigh = gets.chomp.to_i
+weigh = gets.to_i
 puts 'Ваш рост?'
-height = gets.chomp.to_i
+height = gets.to_i
 idealHeigh = 110.to_i
-if weigh <= height - idealHeigh # true
+if weigh <= height - idealHeigh 
 	puts username  +  ', у Вас уже идеальный вес. :)'
-else # false
+else
 	puts username  + ', Идеальный вес для Вас:'
 	puts height - idealHeigh
 end
 
 # Площадь треугольника
 puts 'Каково основание?'
-ohujevanie = gets.chomp.to_f
+ohujevanie = gets.to_f
 puts 'Впишите высоту треуголиника.'
-hueta = gets.chomp.to_f
+hueta = gets.to_f
 puts 'Площадь: '
 puts 1.0 / 2.0 * ohujevanie * hueta
 
 # Прямоугольный треугольник
 puts 'Сторона a?'
-a = gets.chomp.to_i
+a = gets.to_i
 puts 'Сторона b?'
-b = gets.chomp.to_i
+b = gets.to_i
 puts 'Сторона c?'
-c = gets.chomp.to_i
+c = gets.to_i
 
-if a == b && b == c
-	puts 'треугольник равнобедренный и равносторонний'
-elsif a == b && b < c || b == c && c < a || c == a && a < b
-	puts 'треугольник равнобедренный и прямоугольный'
-elsif a > b && b > c || b > c && c > a || c > b && b > a || a > c && c > b || c > a && a > b || b > a && a > c
+[a, b, c]
+
+a, b, c = [a, b, c].sort
+
+if c**2 == a**2+b**2 && a == b
+	puts 'треугольник прямоугольный и равнобедренный'
+elsif c**2 == a**2+b**2 && a != b
 	puts 'треугольник прямоугольный'
-else 
-	puts 'треугольник равнобедренный'
+elsif a == b && b == c
+		puts 'треугольник равносторонний'
+else # другое (например, некорректные данные)
+    puts 'треугольник невалидный'
 end
 
 # Квадратное уравнение
 require "cmath"
 
 puts 'Коэф. a?'
-a = gets.chomp.to_i
+a = gets.to_i
 puts 'Коэф. b?'
-b = gets.chomp.to_i
+b = gets.to_i
 puts 'Коэф. c?'
-c = gets.chomp.to_i
+c = gets.to_i
 
 d = b^2 - 4*a*c
 c = CMath.sqrt(d)
